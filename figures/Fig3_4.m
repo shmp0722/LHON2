fd67f1e49cc8903705d7d3f29d1aee0995ec46e9
shmp0722 <shmp0722@gmail.com>
2014-09-10 13:51:49 -0700

    unite two similar scripts

--- figures/Fig3_4.m
+++ figures/Fig3_4.m
@@ -1,22 +1,19 @@
+function Fig3_4
 % Create figures illustrating the OR, optic tract, FA on the these tracts,
 % and possibly the LGN position
-%
 %
 % Repository dependencies
 %    VISTASOFT
 %    AFQ
 %    LHON2
 %
-% SO Vistasoft lab, 2014
+% SO Vista lab, 2014
 
 %% Identify the directories and subject types in the study
 % The full call can be
-%
-%  [homeDir,subDir,JMD,CRD,LHON,Ctl,RP] = Tama_subj;
-%
 [homeDir,subDir] = Tama_subj;
 
-%% load fiber groups (fg) and ROI files
+%% load fiber groups (fg) and ROI files% select the subject{i}
 
 % This selects a specific
 whichSubject = 23;
@@ -32,40 +29,43 @@ dirOTfg= fullfile(SubDir,'/dwi_2nd/fibers/conTrack/OT_5K');
 dt6 =fullfile(SubDir,'/dwi_2nd/dt6.mat');
 
 % Load fiber groups
+% Optic radiation
 cd(ORfgDir)
-
 fgrOR = fgRead('RORV13mmClipBigNotROI5_clean_clean_D5_L4.mat');
 fglOR = fgRead('LORV13mmClipBigNotROI5_clean_clean_D5_L4.mat');
 
-%% Load optic tract
+% occipital callosal fiber
+% cd(OCFfgDir)
+% fg3 = fgRead('OCF_fsCC_Ctr150_clean.pdb');
 
+% Optic tract
 cd(dirOTfg)
-
 fglOT = 'fg_OT_5K_Optic-Chiasm_Lt-LGN4_2013-08-29_22.32.30-Right-Cerebral-White-Matter_Ctrk100_AFQ_92.pdb';
-fglOT = fgRead(fglOT); 
+fglOT = fgRead(fglOT);
 
 fgrOT = 'fg_OT_5K_Optic-Chiasm_Rt-LGN4_2013-08-29_22.32.30-Left-Cerebral-White-Matter_Ctrk100_AFQ_91.pdb';
 fgrOT = fgRead(fgrOT);
 
+FG = {fgrOR , fglOR, fglOT, fgrOT};
+
 % Load dt6
-dt6 = dtiLoadDt6(dt6);
-t1 = niftiRead(dt6.files.t1);
+dt = dtiLoadDt6(dt6);
+t1 = niftiRead(dt.files.t1);
 
 % load ROIs for Figure 3, but not Figure 4
-dirROI = fullfile(SubDir,'dwi_2nd','ROIs')
+dirROI = fullfile(SubDir,'dwi_2nd','ROIs');
 roiList = {'Optic-Chiasm.mat','Rt-LGN4.mat','Lt-LGN4.mat','lh_V1_smooth3mm_NOT.mat','rh_V1_smooth3mm_NOT.mat'};
 
 %% draw visual pathway figure using AFQ_render
+% Fig4 A
 
-% C = lines(100);
-% c=jet(3);
 % ORs
-mrvNewGraphWin;
-hold on;
+mrvNewGraphWin;hold on;
 
-AFQ_RenderFibers(fgrOR, 'newfig', [0],'numfibers', 100 ,'color', [0.854,0.65,0.125],'radius',[0.5,2]); %fg() = fg
-AFQ_RenderFibers(fglOR, 'newfig', [0],'numfibers', 100 ,'color', [0.854,0.65,0.125],'radius',[0.5,2]); %fg() = fg
-% OCF
+AFQ_RenderFibers(fgrOR, 'newfig', [0],'numfibers', 1000 ,'color', [0.854,0.65,0.125],'radius',[0.5,2]); %fg() = fg
+AFQ_RenderFibers(fglOR, 'newfig', [0],'numfibers', 1000 ,'color', [0.854,0.65,0.125],'radius',[0.5,2]); %fg() = fg
+
+% Occipital callosal fibers
 % AFQ_RenderFibers(fg3, 'newfig', [0],'numfibers', 100 ,'color', [0.4, 0.7, 0.7],'radius',[0.5,2]); %fg() = fg
 
 %Optic Tract
@@ -73,80 +73,83 @@ AFQ_RenderFibers(fglOT, 'newfig', [0],'numfibers', 50 ,'color', [0.67,0.27,0.51]
 AFQ_RenderFibers(fgrOT, 'newfig', [0],'numfibers', 50 ,'color', [0.67,0.27,0.51],'radius',[0.5,2]); %fg() = fg
 
 % % add ROIs if you have the LGN
-theseROIS = [2 3];
-for k = theseROIS
-    Roi = dtiReadRoi(fullfile(dirROI, roiList{k}));
-    AFQ_RenderRoi(Roi);
-end
+% theseROIS = 1:5;
+% for k = theseROIS
+%     Roi = dtiReadRoi(fullfile(dirROI, roiList{k}));
+%     AFQ_RenderRoi(Roi);
+% end
 
-% add T1 anatomy
+% T1w
 AFQ_AddImageTo3dPlot(t1, [0, 0, -30]);
+
+% adjust figure
 view(0 ,89);
 set(gcf,'Color',[1 1 1])
 set(gca,'Color',[1 1 1])
-axis image, axis off
-title('Optic tract and optic radiation')
+axis image
+axis off
 camlight('headlight');
+title('Optic tract and optic radiation')
 hold off;
 
-%% Overlay the FA for this subject on the OR and OT
+%% Fig4 B
+% Overlay the FA for this subject on the OR and OT
 
-% Let's comment this better when BW understands it.
-FG = {fgrOR , fglOR, fglOT, fgrOT};
 % Set the value for overlay to 'fa'
-if(~exist('valName','var') || isempty(valName)), valName = 'fa'; end
+if(~exist('valName','var') || isempty(valName))
+    valName = 'fa'; % 'fa','md','ad', and 'rd' are available. Change value range
+end
 
-mrvNewGraphWin; hold on;
+mrvNewGraphWin;hold on;
 for kk = 1:length(FG)
     % Get the FA values from the fiber group
-    vals = dtiGetValFromFibers(dt6.dt6,FG{kk},inv(dt6.xformToAcpc),valName);
+    vals = dtiGetValFromFibers(dt.dt6,FG{kk},inv(dt.xformToAcpc),valName);
     
-    % Turn the FA values into RGB colors
     rgb = vals2colormap(vals);
-    
     switch kk
         case {1,2}
-            AFQ_RenderFibers(FG{kk},'color',rgb,'crange',[0.3 0.7],'newfig',0,'numfibers',100);
+            AFQ_RenderFibers(FG{kk},'color',rgb,'crange',[0.3 0.7],'newfig',0,'numfibers',1000);
         case {3,4}
             AFQ_RenderFibers(FG{kk},'color',rgb,'crange',[0.3 0.7],'newfig',0);
     end
 end
 
 % Put T1w
-t1 = niftiRead(dt6.files.t1);
+t1 = niftiRead(dt.files.t1);
 AFQ_AddImageTo3dPlot(t1, [0, 0, -30]);
 
 axis image, axis off, view(0,89)
-title('FA overlay on OT/OR')
 colorbar('location','eastoutside');
 caxis([0.3 0.7])
 
-
-%% Render fiber with diffusion measures (FA, MD, AD, RD)
+%% Fig4 C
+% Fa values averaged along tracts
 
 mrvNewGraphWin; hold on;
 
-% Optic radiation
-AFQ_RenderFibers(fgrOR,'dt',dt6,'newfig', 0,'numfibers', 100 ,...
-    'color', [0.854,0.65,0.125],'radius',[0.5,2],'crange',[0.3 0.7 ]);
-AFQ_RenderFibers(fglOR,'dt',dt6, 'newfig', 0,'numfibers', 100 ,...
-    'color', [0.854,0.65,0.125],'radius',[0.5,2],'crange',[0.3 0.7 ]);
-
-% AFQ_RenderFibers(fg3, 'newfig', [0],'numfibers', 100 ,'color', [0.4, 0.7, 0.7],'radius',[0.5,2]); %fg() = fg
-%Optic Tract
-AFQ_RenderFibers(fglOT, 'newfig', 0,'numfibers', 50 ,'color', [0.67,0.27,0.51],'radius',[0.5,2]); %fg() = fg
-AFQ_RenderFibers(fgrOT, 'newfig', 0,'numfibers', 50 ,'color', [0.67,0.27,0.51],'radius',[0.5,2]); %fg() = fg
-
+for kk = 1:length(FG)
+    % TractProfile along current tract
+    TractProfile = SO_FiberValsInTractProfiles(FG{kk},dt,'AP',100,1);
+    
+    % params
+    radius = 3;
+    subdivs = 20;
+    crange = [0.3 0.7];
+    cmap    = 'jet';
+    newfig = 0;
+    
+    % render core fiber with averaged FA values 
+    AFQ_RenderTractProfile(TractProfile.coords.acpc, radius, TractProfile.vals.fa, subdivs, cmap, crange, newfig)
+end
 % Put T1w
-t1 = niftiRead(dt6.files.t1);
 AFQ_AddImageTo3dPlot(t1, [0, 0, -30]);
 
-axis image
-axis off
-view(0,89)
-title('Core fiber with')
-
+axis image, axis off, view(0,89)
+camlight('headlight');
+title('Core fiber with averaged FA value')
+% colorbar('off')
+colorbar('location','eastoutside');
+caxis([0.3 0.7])
 
 %% End
-
 
--- figures/ORfig_Fig4.m
+++ figures/ORfig_Fig4.m
@@ -1,64 +1,83 @@
 function ORfig_Fig4
-% drow figure 4
+% Create figures illustrating the OR, optic tract, FA on the these tracts,
+% and possibly the LGN position
 %
-% Shumpei Ogawa 2014
+% Repository dependencies
+%    VISTASOFT
+%    AFQ
+%    LHON2
+%
+% SO Vista lab, 2014
 
-% Set directory
-[homeDir,subDir,~,~,~,~,~] = Tama_subj;
+%% Identify the directories and subject types in the study
+% The full call can be
+[homeDir,subDir] = Tama_subj;
 
-%% load fg, ROI and dt6 files
-% select the subject{i}
-i =23;
-% directory
-SubDir=fullfile(homeDir,subDir{i});
+%% load fiber groups (fg) and ROI files% select the subject{i}
+
+% This selects a specific
+whichSubject = 23;
+
+% These directories are where we keep the data at Stanford.
+% The pointers must be directed to any site.
+SubDir=fullfile(homeDir,subDir{whichSubject});
 ORfgDir = fullfile(SubDir,'/dwi_2nd/fibers');
-OCFfgDir= fullfile(SubDir,'/dwi_2nd/fibers/conTrack/OCF_Top50K_fsV1V2_3mm');
-OTfgDir= fullfile(SubDir,'/dwi_2nd/fibers/conTrack/conTrack/OT_5K');
+% OCFfgDir= fullfile(SubDir,'/dwi_2nd/fibers/conTrack/OCF_Top50K_fsV1V2_3mm');
+dirOTfg= fullfile(SubDir,'/dwi_2nd/fibers/conTrack/OT_5K');
 
-roiDir = fullfile(SubDir,'/dwi_2nd/ROIs');
+% dirROI = fullfile(SubDir,'/dwi_2nd/ROIs');
 dt6 =fullfile(SubDir,'/dwi_2nd/dt6.mat');
 
+% Load fiber groups
 % Optic radiation
 cd(ORfgDir)
-fg1 = fgRead('RORV13mmClipBigNotROI5_clean_clean_D5_L4.mat');
-fg2 = fgRead('LORV13mmClipBigNotROI5_clean_clean_D5_L4.mat');
+fgrOR = fgRead('RORV13mmClipBigNotROI5_clean_clean_D5_L4.mat');
+fglOR = fgRead('LORV13mmClipBigNotROI5_clean_clean_D5_L4.mat');
 
 % occipital callosal fiber
 % cd(OCFfgDir)
 % fg3 = fgRead('OCF_fsCC_Ctr150_clean.pdb');
 
-cd(OTfgDir)
+% Optic tract
+cd(dirOTfg)
+fglOT = 'fg_OT_5K_Optic-Chiasm_Lt-LGN4_2013-08-29_22.32.30-Right-Cerebral-White-Matter_Ctrk100_AFQ_92.pdb';
+fglOT = fgRead(fglOT);
 
-fg4 = 'fg_OT_5K_Optic-Chiasm_Lt-LGN4_2013-08-29_22.32.30-Right-Cerebral-White-Matter_Ctrk100_AFQ_92.pdb';
-fg5 = 'fg_OT_5K_Optic-Chiasm_Rt-LGN4_2013-08-29_22.32.30-Left-Cerebral-White-Matter_Ctrk100_AFQ_91.pdb';
-fg4 = fgRead(fg4); fg5 = fgRead(fg5);
+fgrOT = 'fg_OT_5K_Optic-Chiasm_Rt-LGN4_2013-08-29_22.32.30-Left-Cerebral-White-Matter_Ctrk100_AFQ_91.pdb';
+fgrOT = fgRead(fgrOT);
 
-FG = {fg1 , fg2, fg4, fg5};
+FG = {fgrOR , fglOR, fglOT, fgrOT};
+
 % Load dt6
 dt = dtiLoadDt6(dt6);
 t1 = niftiRead(dt.files.t1);
 
-% Create TractProfile
-for kk = 1:length(FG)
-    TractProfile{kk} = SO_FiberValsInTractProfiles(FG{kk},dt,'AP',100,1);
-end
+% load ROIs for Figure 3, but not Figure 4
+dirROI = fullfile(SubDir,'dwi_2nd','ROIs');
+roiList = {'Optic-Chiasm.mat','Rt-LGN4.mat','Lt-LGN4.mat','lh_V1_smooth3mm_NOT.mat','rh_V1_smooth3mm_NOT.mat'};
 
-% % load ROIs
-% roi = {'Optic-Chiasm.mat','Rt-LGN4.mat','Lt-LGN4.mat','lh_V1_smooth3mm_NOT.mat','rh_V1_smooth3mm_NOT.mat'};
-
-
-%% Fig4 A
+%% draw visual pathway figure using AFQ_render
+% Fig4 A
 
 % ORs
-figure; subplot(1,3,1);
-hold on;
-AFQ_RenderFibers(fg1, 'newfig', [0],'numfibers', 1000 ,'color', [0.854,0.65,0.125],'radius',[0.5,2]); %fg() = fg
-AFQ_RenderFibers(fg2, 'newfig', [0],'numfibers', 1000 ,'color', [0.854,0.65,0.125],'radius',[0.5,2]); %fg() = fg
+mrvNewGraphWin;hold on;
 
+AFQ_RenderFibers(fgrOR, 'newfig', [0],'numfibers', 1000 ,'color', [0.854,0.65,0.125],'radius',[0.5,2]); %fg() = fg
+AFQ_RenderFibers(fglOR, 'newfig', [0],'numfibers', 1000 ,'color', [0.854,0.65,0.125],'radius',[0.5,2]); %fg() = fg
+
+% Occipital callosal fibers
 % AFQ_RenderFibers(fg3, 'newfig', [0],'numfibers', 100 ,'color', [0.4, 0.7, 0.7],'radius',[0.5,2]); %fg() = fg
+
 %Optic Tract
-AFQ_RenderFibers(fg4, 'newfig', [0],'numfibers', 50 ,'color', [0.67,0.27,0.51],'radius',[0.5,2]); %fg() = fg
-AFQ_RenderFibers(fg5, 'newfig', [0],'numfibers', 50 ,'color', [0.67,0.27,0.51],'radius',[0.5,2]); %fg() = fg
+AFQ_RenderFibers(fglOT, 'newfig', [0],'numfibers', 50 ,'color', [0.67,0.27,0.51],'radius',[0.5,2]); %fg() = fg
+AFQ_RenderFibers(fgrOT, 'newfig', [0],'numfibers', 50 ,'color', [0.67,0.27,0.51],'radius',[0.5,2]); %fg() = fg
+
+% % add ROIs if you have the LGN
+% theseROIS = 1:5;
+% for k = theseROIS
+%     Roi = dtiReadRoi(fullfile(dirROI, roiList{k}));
+%     AFQ_RenderRoi(Roi);
+% end
 
 % T1w
 AFQ_AddImageTo3dPlot(t1, [0, 0, -30]);
@@ -70,17 +89,22 @@ set(gca,'Color',[1 1 1])
 axis image
 axis off
 camlight('headlight');
-
+title('Optic tract and optic radiation')
 hold off;
 
 %% Fig4 B
+% Overlay the FA for this subject on the OR and OT
+
+% Set the value for overlay to 'fa'
 if(~exist('valName','var') || isempty(valName))
-    valName = 'fa';
+    valName = 'fa'; % 'fa','md','ad', and 'rd' are available. Change value range
 end
 
-subplot(1,3,2);hold on;
+mrvNewGraphWin;hold on;
 for kk = 1:length(FG)
+    % Get the FA values from the fiber group
     vals = dtiGetValFromFibers(dt.dt6,FG{kk},inv(dt.xformToAcpc),valName);
+    
     rgb = vals2colormap(vals);
     switch kk
         case {1,2}
@@ -94,32 +118,38 @@ end
 t1 = niftiRead(dt.files.t1);
 AFQ_AddImageTo3dPlot(t1, [0, 0, -30]);
 
-axis image
-axis off
-% adjust view and give title
-view(0,89)
+axis image, axis off, view(0,89)
+colorbar('location','eastoutside');
+caxis([0.3 0.7])
 
 %% Fig4 C
-subplot(1,3,3); hold on;
+% Fa values averaged along tracts
+
+mrvNewGraphWin; hold on;
+
 for kk = 1:length(FG)
+    % TractProfile along current tract
+    TractProfile = SO_FiberValsInTractProfiles(FG{kk},dt,'AP',100,1);
+    
+    % params
     radius = 3;
     subdivs = 20;
     crange = [0.3 0.7];
     cmap    = 'jet';
     newfig = 0;
     
-    % drow picture
-    AFQ_RenderTractProfile(TractProfile{kk}.coords.acpc, radius, TractProfile{kk}.vals.fa, subdivs, cmap, crange, newfig)
+    % render core fiber with averaged FA values 
+    AFQ_RenderTractProfile(TractProfile.coords.acpc, radius, TractProfile.vals.fa, subdivs, cmap, crange, newfig)
 end
 % Put T1w
 AFQ_AddImageTo3dPlot(t1, [0, 0, -30]);
 
-axis image
-axis off
-% adjust view
-view(0,89)
+axis image, axis off, view(0,89)
 camlight('headlight');
+title('Core fiber with averaged FA value')
+% colorbar('off')
+colorbar('location','eastoutside');
+caxis([0.3 0.7])
 
-colorbar('off')
-hold off
+%% End
 
